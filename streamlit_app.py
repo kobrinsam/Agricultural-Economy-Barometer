b0a47d80fbccc95a833c2873cce032dfaf9f99a5
kobrinsam <60291549+kobrinsam@users.noreply.github.com>
2022-08-31 03:04:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import yfinance as yf
 from sklearn import preprocessing
 import numpy as np
 st.title("American Agricultural Economy Barometer")
-st.subheader("Providing a sense of the health of the agriculture industry on a monthly basis")
+st.subheader("Surveying the state of the agriculture industry on a monthly basis")
 st.caption("Data sources include Ag Economy Barometer Indices produced by Perdue Univeristy and key Agricultural input and output prices.")
 ### format ag barometer data
 df = pd.read_html("https://ag.purdue.edu/commercialag/ageconomybarometer/tables/")[0]
@@ -47,15 +47,18 @@ total_data = total_data.set_index('Date')
 scaled_df = preprocessing.normalize(total_data.fillna(0), axis=0)
 scaled_df = pd.DataFrame(scaled_df, columns=total_data.columns, index =total_data.index).replace({0:np.NaN}).reset_index()
 total_data = total_data.reset_index()
-
+barometer_list = ['Purdue/CME Ag Economy Barometer',
+ 'Index of Current Conditions',
+ 'Index of Future Expectations',
+ 'Farm Capital Investment Index']
 ####
 # correlations df
-corr_df = scaled_df.rename(columns={"DC=F":"Milk Futures (DC-F)", "HE=F" :"Lean Hog Futures (HE=F)",
+corr_df = scaled_df[scaled_df['Date']>'2010-10-01'].rename(columns={"DC=F":"Milk Futures (DC-F)", "HE=F" :"Lean Hog Futures (HE=F)",
                               "LE=F": "Live Cattle Futures (LE=F)",
                               "UFV=F":"Urea (Granular) FOB US Gulf Fut (UFV=F)",
                               "ZS=F":"Soybean Futures,Nov-2022 (ZS=F)",
                               "NG=F": "Natural Gas Oct 22 (NG=F)",
-                              "ZC=F":"Corn Futures,Dec-2022 (ZC=F)"}).corr()
+                              "ZC=F":"Corn Futures,Dec-2022 (ZC=F)"}).corr().drop(barometer_list, axis=0)
 ###functions
 def display_data(norm):
     if norm:
@@ -66,7 +69,9 @@ def display_data(norm):
 
 indicators = st.multiselect('Agricultural Economic Indicators', list(total_data.drop('Date', axis=1).columns))
 norm = st.checkbox('Normalize Economic Indicators')
+st.text("Ag Economy Indicators, by month")
 st.line_chart(data = display_data(norm), x= 'Date' , y = indicators)
+st.text("Ag Economy Indicator Data Table")
 st.dataframe(display_data(norm).set_index('Date'))
 st.subheader('Historically, Farmer Setiment Is Assocated with Key Agricultural Commodity Prices')
 st.text('Correlation Between Ag Economy Barometer Indices and Key Ag Commodity Prices')
